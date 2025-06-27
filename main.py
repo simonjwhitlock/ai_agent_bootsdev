090f183f47db564407fcc46a091e97a0ee9f4fff
simon whitlock <simonjwhitlock@gmail.com>
2025-06-27 11:27:57 +0100

    shows which functions it called

--- main.py
+++ main.py
@@ -4,30 +4,67 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-#SYSTEM PROMPT and rules
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
-
+#load API key
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+#create client object
 client = genai.Client(api_key=api_key)
 
+#test for user prompt when script is called
 try:
     user_prompt = sys.argv[1]
 except:
     print("there is no argument")
     exit(1)
+    
+#set available options
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
+#set system prompt
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+#load user argument in to prompts message format
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
+#make call to genai and store response then print it with verbose options if flag set
 response = client.models.generate_content(
     model="gemini-2.0-flash-001", 
     contents = messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt)
+    config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt)
     )
-print(response.text)
+if len(response.function_calls) > 0:
+   for function_call_part in response.function_calls:
+       print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+    print(response.text)
 
 if any([arg=='-v' or arg=='--verbose' for arg in sys.argv]):
     print(f"User prompt: {user_prompt}")
