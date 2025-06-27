0c1cf557424abfa0848c6ff26de889b4df0d0c9e
simon whitlock <simonjwhitlock@gmail.com>
2025-06-27 11:09:00 +0100

    yes we know your just a robot! - system prompt set to shoud "I'M JUST A ROBOT"

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+#SYSTEM PROMPT and rules
+system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -19,7 +22,11 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents = messages)
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001", 
+    contents = messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt)
+    )
 print(response.text)
 
 if any([arg=='-v' or arg=='--verbose' for arg in sys.argv]):
